1f8f5d4cd4766428ef86e670a58de5d15a43406b
gautamkumar1 <gautamraaz936@gmail.com>
2024-12-31 17:44:03 +0530

    undo changes

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "https://devmusic-backend.onrender.com/api/:path*", 
+        destination: "http://localhost:3001/api/:path*", 
       },
     ];
   },
